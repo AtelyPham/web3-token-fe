5f0719864fcc241e25aa4a1a24c1dbd8dd94aec3
AtelyPham <tinpham1412000@gmail.com>
2022-01-20 10:46:38 +0700

    fix: 🐛 change the object option to sign function

--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,5 @@
-import Web3 from 'web3'
-import Web3Token from 'web3-token'
+import Web3 from "web3"
+import Web3Token from "web3-token"
 
 export const sign = async expireTimeInSecond => {
   const web3 = new Web3(window.ethereum)
@@ -9,7 +9,7 @@ export const sign = async expireTimeInSecond => {
 
   const token = await Web3Token.sign(
     msg => web3.eth.personal.sign(msg, address),
-    `${expireTimeInSecond}s`
+    { expires_in: `${expireTimeInSecond}s` }
   )
 
   return token
